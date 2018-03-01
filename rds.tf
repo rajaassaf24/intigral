474cf24e7c75ce6c09149347ae0a4cf417f25006
Raja Assaf <raja.assaf@kirontech.com>
2018-03-01 15:13:07 +0200

    change mysql version, run rds in its own availability zone, create db_subnet_group required by RDS in order to launch the instance in the valid VPC and not the default one

--- rds.tf
+++ rds.tf
@@ -2,11 +2,19 @@ resource "aws_security_group" "rds" {
   name = "rds"
   description = "Allow connections to rds from wordpress Instances"
   vpc_id = "${aws_vpc.wordpress.id}"
-
 # keeping the instances and RDS on the same vpc. Can configure different vpc and# use vpc peering see "https://www.terraform.io/docs/providers/aws/r/vpc_peering.html" for more details
-
   tags {
     Name = "RDS"
+    Group = "Intigral"
+  }
+}
+
+resource "aws_db_subnet_group" "rds" {
+  name = "rds"
+  subnet_ids = ["${aws_subnet.wordpress.id}","${aws_subnet.rds.id}"]
+
+  tags {
+    Name = "RDS DB subnet"
     Group = "Intigral"
   }
 }
@@ -17,27 +25,31 @@ resource "aws_security_group_rule" "rds_wordpress_in" {
   protocol = "tcp"
   from_port = 3306 
   to_port = 3306
+  source_security_group_id = "${aws_security_group.wordpress.id}"
 }
 
 resource "aws_security_group_rule" "rds_wordpress_out" {
-  security_group_id = "${aws_security_group.wordpress.id}"
+  security_group_id = "${aws_security_group.rds.id}"
   type = "egress"
   protocol = "tcp"
   from_port = 3306
   to_port = 3306
+  source_security_group_id = "${aws_security_group.wordpress.id}"
 }
 
 resource "aws_db_instance" "rds" {
   identifier = "raja"
   allocated_storage = "10"
   engine = "mysql"
-  engine_version = "5.6.22"
+  engine_version = "5.6.37"
   instance_class = "db.t2.micro"
+  availability_zone = "${local.rds_infra_az}"
   multi_az = "false"
   name = "wordpress"
   username = "wordpress"
   password = "password"
-  vpc_security_group_ids = ["${aws_security_group.wordpress.id}"]
+  vpc_security_group_ids = ["${aws_security_group.rds.id}"]
+  db_subnet_group_name = "${aws_db_subnet_group.rds.id}"
 
   tags {
     Name = "WordpressRDS"
