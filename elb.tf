120cebe7c2332562665475c26108f497100c88b5
Raja Assaf <raja.assaf@kirontech.com>
2018-03-05 17:57:12 +0200

    change the healthcheck interval to 10 instead of 30 and ping tcp port 80

--- elb.tf
+++ elb.tf
@@ -13,8 +13,8 @@ resource "aws_elb" "wordpress" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:80/"
-    interval            = 30
+    target              = "TCP:80"
+    interval            = 10
   }
 
   instances                   = ["${aws_instance.wordpress.*.id}"]
